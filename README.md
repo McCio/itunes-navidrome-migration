d319a847cef977a0cd60896950d646f29d181705
Marco Ciotola <github@ciotola.dev>
2026-03-08 16:34:40 +0100

    feat: merge new stats instead of overwriting
    feat: support user selection when multiple on navidrome
    feat: some more 'fuzzy' file matching
    feat: support latest navidrome annotation table
    chore: more informative logging

--- README.md
+++ README.md
@@ -1,33 +1,32 @@
 # itunes-navidrome-migration
-Python scripts to transfer iTunes history to a new Navidrome installation
+Python scripts to transfer iTunes history to a Navidrome installation
+
 ## Introduction
-These Python scripts will transfer song ratings, play counts, play dates and playlists from an existing iTunes library to a new Navidrome installation.
+These Python scripts will transfer song ratings, play counts, play dates and playlists from an existing iTunes library to a Navidrome installation.
 
 ## Background
-Itunes saves its data in a Library.xml file. Navidrome saves its data in up to three different `navidrome.db*` files. The script reads from `Library.xml` and writes the data to the navidrome.db file.
+iTunes saves its data in a Library.xml file. Navidrome saves its data in up to three different `navidrome.db*` files. The script reads from `Library.xml` and writes the data to the navidrome.db file.
 
-These scripts were tested on Linux using a Library.xml file that came from iTunes on Windows. I'm running the Docker version of Navidrome v0.48.0 & Python version 3.10.6.
+These scripts were tested on Linux via [uv](https://docs.astral.sh/uv/) using a Library.xml file that came from iTunes on Windows. I'm running the Docker version of Navidrome v0.60.3 & uv version 0.10.6.
 
 I am not sure how it would work on other platforms.
 
 ## Known issues
-1. If the iTunes database has a star rating for an entry but no play count or play date, then it will not migrate that rating to Navidrome. I'm not sure if this is a common enough situation to bother fixing in the ratings migration script. Let me know if it affects you.
-
-2. Foreign alphabets and characters (e.g. Japanese) may not transfer. See [this issue](https://github.com/Stampede/itunes-navidrome-migration/issues/4) for details.
+1. Foreign alphabets and characters (e.g. Japanese) may not transfer. See [this issue](https://github.com/Stampede/itunes-navidrome-migration/issues/4) for details.
 
 ## Installation
-1. You can create a Python virtual environment, or not.
-2. Download `itunesPlaylistMigrator.py`, `itunestoND.py` and `requirements.txt` and save to a folder.
-3. `$ pip3 install -r requirements.txt`
+1. I suggest installing [uv](https://docs.astral.sh/uv/).
+2. Download `itunesPlaylistMigrator.py`, `itunestoND.py`.
+3. `$ chmod +x itunesPlaylistMigrator.py itunestoND.py`
+4. The scripts are ready to execute like `$ ./itunestoND.py`
 
-### Not comfortable with Python?
-Put your database files in a drop box or something and I will migrate your iTunes library for $15. Email me at ylh9hhiaq@mozmail.com. For playlists, I'd send you back a web page with your playlists listed and hotlinked. If you want to keep the playlist, just click on its link and it will get added to Navidrome.
+If you do not want to use uv, check one of the two scripts for the dependencies to install.
 
 ## How to use
 ### Preparing your library
 Set up your Navidrome server and copy all the folders and music files from your iTunes library to the Navidrome library. Navidrome will build its own database from scratch based on the file metadata. 
 
-The most important thing is that you keep the same directory structure between iTunes and Navidrome libraries. Do not rename, delete or move any files or directories. The script uses the file paths to sync the databases. If you want to reorganize the file structure, do it after you have moved over all your itunes data.
+The most important thing is that you keep the same directory structure between iTunes and Navidrome libraries. Do not rename, delete or move any files or directories. The script uses the file paths to sync the databases. If you want to reorganize the file structure, do it after you have moved over all your iTunes data.
 
 That said, before running the scripts, I found it very helpful to use Music Brainz Picard to clean up file metadata **without moving any files**. Use Navidrome for a week or so and if you have problems finding albums or songs, use Picard or Beets or something to improve the metadata tags for the files that are acting funny.
 
@@ -36,12 +35,15 @@ That said, before running the scripts, I found it very helpful to use Music Brai
 ### Migrating play counts, last played date and song ratings
 1. Shut down your Navidrome server.
 2. Copy the Navidrome database files to the machine with these scripts. In my case there are 3 database files: `navidrome.db`, `navidrome.db-shm` and `navidrome.db-wal` you need any `navidrome.db*` file that you find.
-3. Run the first script: `$python3 itunestoND.py`
+3. Run the first script: `$ ./itunestoND.py`
 4. It will prompt you to type the path to the `navidrome.db` and `Library.xml`
-5. Wait. For large libraries, it can take a few minutes to crunch all the data in `Library.xml`.
-6. When it's done, your Navidrome database files may be collapsed into a single `navidrome.db` file. This is OK.
-7. **On the machine with the ND server:** delete the 3 database files, then copy over the `navidrome.db` file from the script. Put it in their place.
-8. Start your Navidrome server to make sure everything worked correctly. You should now have song ratings and play counts.
+   - *in alternative, you can run directly the script with the two paths as arguments, first navidrome and the iTunes* 
+5. If you already setup multiple users in navidrome, it will prompt you to choose which user to apply the migration to.
+6. Wait. For large libraries, it can take a few minutes to crunch all the data in `Library.xml`.
+7. After loading and matching all data, you will see a summary and be asked for confirmation to apply the migration to `navidrome.db`.
+8. When it's done, your Navidrome database files may be collapsed into a single `navidrome.db` file. This is OK.
+9. **On the machine with the ND server:** delete the 3 database files, then copy over the `navidrome.db` file from the script. Put it in their place.
+10. Start your Navidrome server to make sure everything worked correctly. You should now have song ratings and play counts.
 
 The script will also generate a file called `IT_file_correlations.py`. If you don't want to move over your iTunes playlists, you can just delete this.
 
@@ -56,7 +58,9 @@ I wrote this as an afterthought, so these instructions are a little weird. This 
 6. The script will search for playlists from your iTunes library and prompt if you want to move them to Navidrome.
 
 ## Acknowledgments
-Thanks to the Navidrome developers for their hard work and for putting up with my basic questions on the Discord chat as I worked on this script.
+Thanks to [Stampede](https://github.com/Stampede) for the first version of the script.
+
+Thanks to all the Navidrome developers for their hard work.
 
 Hopefully these scripts help some people. Feel free to copy / share / improve etc.. As far as I'm concerned, this is public domain.
 
--- itunestoND.py
+++ itunestoND.py
@@ -1,172 +1,438 @@
-#!/usr/bin/env python
+#!/usr/bin/env -S uv run --script
+#
+# /// script
+# requires-python = "<3.13"
+# dependencies = [
+#     "beautifulsoup4<5",
+#     "bs4==0.0.1",
+#     "certifi",
+#     "charset-normalizer<3",
+#     "idna<4",
+#     "lxml<5",
+#     "pyinputplus<1",
+#     "pysimplevalidate<1",
+#     "requests<3",
+#     "soupsieve<3",
+#     "stdiomask<0.1",
+#     "urllib3<2",
+# ]
+# ///
+
 
 # itunestoND.py - Transfers song ratings, playcounts and play dates from I-Tunes library
 # to the Navidrome database
 
-import sys, sqlite3, datetime, re, string, pprint, random
+import datetime
+import pprint
+import sqlite3
+import sys
+from dataclasses import dataclass
 from pathlib import Path
 from urllib.parse import unquote
+
 from bs4 import BeautifulSoup
 
-def get_db_path(dbID):
-    while True:
-        path = Path(input('Enter the path to the %s: ' % dbID))
+
+def max_date(d1, d2):
+    if d1 is None:
+        return d2
+    if d2 is None:
+        return d1
+    return max(d1, d2)
+
+
+@dataclass
+class Stats:
+    total: int = 0
+    seen: int = 0
+    skip_no_location: int = 0
+    skip_not_found_video: int = 0
+    skip_not_found: int = 0
+    skip_duplicated: int = 0
+
+
+@dataclass
+class PlayStat:
+    nd_exists: bool = False
+    nd_play_count: int = 0
+    nd_play_date: datetime.datetime = None
+    nd_rating: int = 0
+    nd_rated_at: datetime.datetime = None
+    it_play_count: int = 0
+    it_play_date: datetime.datetime = None
+    it_rating: int = 0
+    it_rated_at: datetime.datetime = None
+    it_album_rating: int = 0
+
+    def play_count(self):
+        return self.it_play_count + self.nd_play_count
+
+    def play_date(self):
+        return max_date(self.it_play_date, self.nd_play_date)
+
+    def rating(self):
+        if self.nd_rating == 0:
+            return self.it_rating
+        return self.nd_rating
+
+    def rated_at(self):
+        return max_date(self.it_rated_at, self.nd_rated_at)
+
+
+def get_db_path(dbID, argN):
+    if len(sys.argv) > argN:
+        path = Path(sys.argv[argN]).resolve()
         if not path.is_file():
-            print(str(path) + ' is not a file. Try again.')
-        else: break
+            print(str(path) + " is not a file.")
+        else:
+            return path
+    while True:
+        path = Path(input("Enter the path to the %s: " % dbID)).resolve()
+        if not path.is_file():
+            print(str(path) + " is not a file. Try again.")
+        else:
+            break
     return path
+
 
 def determine_userID(nd_p):
     conn = sqlite3.connect(nd_p)
     cur = conn.cursor()
-    cur.execute('SELECT id, user_name FROM user')
+    cur.execute("SELECT id, user_name FROM user")
     users = cur.fetchall()
     if len(users) == 1:
-        print(f'Changes will be applied to the {users[0][1]} Navidrome account.')
+        print(
+            f"Changes will be applied to the {users[0][1]} Navidrome account."
+        )
     else:
-        raise Exception('There needs to be exactly one user account set up with Navidrome. You either have 0, or more than 1 user account.')
+        while True:
+            if len(users) == 0:
+                print("There are no user accounts set up with Navidrome.")
+                raise Exception(
+                    "There needs to be at least one user account set up with Navidrome."
+                )
+            else:
+                print("There are multiple user accounts set up with Navidrome.")
+            print("Please select the user account to apply changes to:")
+            for i, user in enumerate(users):
+                print(f"{i + 1}. {user[1]}")
+            choice = input("Enter the number of the user account: ")
+            try:
+                choice = int(choice)
+                if 1 <= choice <= len(users):
+                    return users[choice - 1][0], users[choice - 1][1]
+            except ValueError:
+                pass
+            print("Invalid choice. Try again.")
     conn.close()
-    return users[0][0]
+    return users[0][0], users[0][1]
 
-def update_playstats(d1, id, playcount, playdate, rating=0):
-    d1.setdefault(id, {})
-    d1[id].setdefault('play count', 0)
-    d1[id].setdefault('play date', datetime.datetime.fromordinal(1))
-    d1[id]['play count'] += playcount
-    d1[id]['rating'] = rating
 
-    if playdate > d1[id]['play date']: d1[id].update({'play date': playdate})
+FIND_SONG_QUERY = 'SELECT f.id, f.artist_id, f.album_id, u.play_count, u.play_date, u.rating, u.rated_at FROM media_file f LEFT JOIN annotation u ON u.user_id = ? AND u.item_id = f.id AND u.item_type = "media_file" WHERE INSTR(f.path, ?) > 0'
 
-def generate_annotation_id(): # random hex number 32 characters long
-    character_pool = string.hexdigits[:16]
 
-    id_elements = []
-    for char_count in (8, 4, 4, 4, 12):
-        id_elements.append(''.join(random.choice(character_pool) for i in range(char_count)))
-    return '-'.join(id_elements)
+def find_song(cur, song_path):
+    cur.execute(FIND_SONG_QUERY, (userID, song_path))
+    fetched = cur.fetchone()
+    while fetched is None:
+        if "/" in song_path:
+            song_path = "/".join(*song_path.split("/")[1:])
+        else:
+            return fetched
+        cur.execute(FIND_SONG_QUERY, (userID, song_path))
+        fetched = cur.fetchone()
+    return fetched
+
+
+def get_itunes_local_path(song_path, it_root):
+    if song_path.is_relative_to(it_root):
+        return song_path.relative_to(it_root).as_posix()
+    if it_root.stem == "iTunes Music":
+        it_root = it_root.parent
+        if it_root.stem == "iTunes":
+            if song_path.is_relative_to(it_root.parent):
+                return song_path.relative_to(it_root.parent).as_posix()
+        if song_path.is_relative_to(it_root):
+            return song_path.relative_to(it_root).as_posix()
+    return None
+
+
+def as_dt(val):
+    if val is None:
+        return None
+    try:
+        return datetime.datetime.strptime(val[:19], "%Y-%m-%dT%H:%M:%S")
+    except ValueError:
+        return datetime.datetime.strptime(val[:19], "%Y-%m-%d %H:%M:%S")
+
+
+def extract_date_key(it_song_entry, key):
+    date_val = it_song_entry.find("key", string=key)
+    if date_val is None:
+        return None
+    date_val = date_val.next_sibling
+    if date_val is None:
+        return None
+    # convert from string to datetime object. Example string: '2020-01-19T02:24:14Z'
+    return as_dt(date_val.text)
+
+
+def extract_int_key(it_song_entry, key, default=0):
+    val = it_song_entry.find("key", string=key)
+    if val is None:
+        if default is None:
+            raise ValueError(f"Key '{key}' not found")
+        return default
+    val = val.next_sibling
+    if val is None:
+        if default is None:
+            raise ValueError(f"Key '{key}' not found")
+        return default
+    try:
+        return int(val.text)
+    except ValueError as e:
+        if default is not None:
+            return default
+        raise e
+
+
+def update_playstats(d1, id, song_info, cur, item_type):
+    if id not in d1:
+        d1[id] = PlayStat()
+        cur.execute(
+            "SELECT a.play_count, a.play_date, a.rating, a.rated_at FROM annotation a WHERE a.user_id = ? AND a.item_id = ? AND a.item_type = ?",
+            (userID, id, item_type),
+        )
+        try:
+            (
+                nd_play_count,
+                nd_play_date,
+                nd_rating,
+                nd_rated_at,
+            ) = cur.fetchone()
+            d1[id].nd_exists = True
+            d1[id].nd_play_count = nd_play_count or 0
+            d1[id].nd_play_date = as_dt(nd_play_date)
+            d1[id].nd_rating = nd_rating or 0
+            d1[id].nd_rated_at = as_dt(nd_rated_at)
+        except TypeError:
+            pass
+    d1[id].it_album_rating = song_info.it_album_rating
+    d1[id].it_play_count += song_info.it_play_count
+    d1[id].it_play_date = max_date(d1[id].it_play_date, song_info.it_play_date)
+    d1[id].it_rated_at = max_date(d1[id].it_rated_at, song_info.it_rated_at)
+
 
 def write_to_annotation(dictionary_with_stats, entry_type):
-    annotation_entries = []
+    new_annotations = []
+    update_annotations = []
     for item_id in dictionary_with_stats:
         this_entry = dictionary_with_stats[item_id]
-        
-        play_count = this_entry['play count']
-        play_date = this_entry['play date'].strftime('%Y-%m-%d %H:%M:%S') # YYYY-MM-DD 24:mm:ss
-        rating = this_entry['rating']
 
-        annotation_entries.append((generate_annotation_id(), userID, item_id, entry_type, play_count, play_date, rating, 0, None))
+        play_date = this_entry.play_date()
+        if play_date is not None:
+            play_date = play_date.strftime("%Y-%m-%d %H:%M:%S")
+
+        rated_date = this_entry.rated_at()
+        if rated_date is not None:
+            rated_date = rated_date.strftime("%Y-%m-%d %H:%M:%S")
+
+        if (
+            this_entry.play_count() == 0
+            and this_entry.rating() == 0
+            and this_entry.nd_exists is False
+        ):
+            continue
+
+        annotation = (
+            userID,
+            item_id,
+            entry_type,
+            this_entry.play_count(),
+            play_date,
+            this_entry.rating(),
+            rated_date,
+        )
+        if this_entry.nd_exists:
+            update_annotations.append(annotation)
+        else:
+            new_annotations.append(annotation)
 
     conn = sqlite3.connect(nddb_path)
     cur = conn.cursor()
-    cur.executemany('INSERT INTO annotation VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', annotation_entries)
+    if new_annotations:
+        cur.executemany(
+            "INSERT INTO annotation(user_id,item_id,item_type,play_count,play_date,rating,rated_at) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            new_annotations,
+        )
+        print(f"{len(new_annotations)} new annotations inserted.")
+    if update_annotations:
+        cur.executemany(
+            "UPDATE annotation SET play_count=?, play_date=?, rating=?, rated_at=? WHERE user_id=? AND item_id=? AND item_type=?",
+            update_annotations,
+        )
+        print(f"{len(update_annotations)} annotations updated.")
     conn.commit()
     conn.close()
-        # cur.executemany('INSERT INTO consumers VALUES (?,?,?,?)', purchases)
-        # cur.execute("INSERT INTO consumers VALUES (1,'John Doe','john.doe@xyz.com','A')")
 
-_ = None
-while _ != 'proceed':
-    print()
-    print('This script will migrate certain data from your ITunes library to your Navidrome database.', 
-        "Back up all your data in case it doesn't work properly on your setup. NO WARRANTIES. NO PROMISES.", 
-        "The script will DELETE existing data you have in your Navidrome database so it can start \"fresh\".", sep='\n')
-    print()
 
-    _ = input('Type PROCEED to continue, or Q to quit: ').lower()
+nddb_path = get_db_path("Navidrome database", 1)
+userID, userName = determine_userID(nddb_path)
 
-    if _ == 'q': print('Good bye.'); sys.exit(0)
+itdb_path = get_db_path("iTunes database", 2)
 
-nddb_path = get_db_path('Navidrome database')
-itdb_path = get_db_path('Itunes database')
-print('\nParsing Itunes library. This may take a while.')
-with open(itdb_path, 'r', encoding="utf-8") as f: soup = BeautifulSoup(f, 'lxml-xml')
+print("\nParsing iTunes library. This may take a while.")
+with open(itdb_path, "r", encoding="utf-8") as f:
+    soup = BeautifulSoup(f, "lxml-xml")
 
-it_root_music_path = unquote(soup.find('key', text='Music Folder').next_sibling.text)
+it_root_music_path = Path(
+    unquote(soup.find("key", string="Music Folder").next_sibling.text)
+)
 # example output of previous line: 'file://localhost/C:/Users/REDACTED/Music/iTunes/iTunes Music/'
 
-songs = soup.dict.dict.find_all('dict') # yields result set of media files to loop through
+songs = soup.dict.dict.find_all(
+    "dict"
+)  # yields result set of media files to loop through
 
 song_count = len(songs)
-print(f'Found {song_count:,} files in Itunes database to process.')
-del(soup)
+print(f"Found {song_count} files in iTunes database to process.")
+del soup
 
-userID = determine_userID(nddb_path)
-songID_correlation = {} # we'll save this for later use to transfer Itunes playlists to ND (another script)
-artists = {}            # artists and albums will keep count of plays and play dates for each
+songID_correlation = {}  # we'll save this for later use to transfer iTunes playlists to ND (another script)
+artists = {}  # artists and albums will keep count of plays and play dates for each
 albums = {}
 files = {}
 
 
 status_interval = song_count // 8
-counter = 0
+stats = Stats()
+stats.total = song_count
 
 conn = sqlite3.connect(nddb_path)
 cur = conn.cursor()
-cur.execute('DELETE FROM annotation')
-conn.commit()
 
 for it_song_entry in songs:
-    counter += 1    # progress tracking feedback
-    if counter % status_interval == 0:
-        print(f'{counter:,} files parsed so far of {song_count:,} total songs.')
+    stats.seen += 1
+    if stats.seen % status_interval == 0:
+        print(
+            f"{stats.seen:,} files parsed so far of {song_count:,} total songs."
+        )
+        print("Current stats:", stats)
 
     # chop off first part of IT path so we can correlate it to the entry in the ND database
-    
-    if it_song_entry.find('key', string='Location') == None: 
+
+    if it_song_entry.find("key", string="Location") is None:
+        stats.skip_no_location += 1
         continue
 
-    song_path = unquote(it_song_entry.find('key', string='Location').next_sibling.text)
-    if not song_path.startswith(it_root_music_path):  # excludes non-local content
-        continue   
-
-    song_path = re.sub(it_root_music_path, '', song_path)
+    song_path = Path(
+        unquote(it_song_entry.find("key", string="Location").next_sibling.text)
+    )
+    local_path = get_itunes_local_path(song_path, it_root_music_path)
+    if local_path is None:
+        song_path = song_path.as_posix()
+    else:
+        song_path = local_path
 
     try:
-        cur.execute('SELECT id, artist_id, album_id FROM media_file WHERE path LIKE "%' + song_path + '"')
-        song_id, artist_id, album_id = cur.fetchone()
+        (
+            song_id,
+            artist_id,
+            album_id,
+            nd_play_count,
+            nd_play_date,
+            nd_rating,
+            nd_rated_at,
+        ) = find_song(cur, song_path)
+        song_info = PlayStat(
+            nd_exists=nd_play_count is not None,
+            nd_play_count=nd_play_count or 0,
+            nd_play_date=as_dt(nd_play_date),
+            nd_rating=nd_rating or 0,
+            nd_rated_at=as_dt(nd_rated_at),
+        )
     except TypeError:
-        print(f"Error while parsing {song_path}. Navidrome does not acknowledge that file's existence.")
-        print("Maybe Navidrome doesn't like the extension? Skipping.")
+        if (
+            str(song_path).endswith("mp4")
+            or str(song_path).endswith("m4v")
+            or str(song_path).endswith("mov")
+            or str(song_path).endswith("mpg")
+        ):
+            stats.skip_not_found_video += 1
+            continue
+        print(f"NOTF: {song_path}")
+        stats.skip_not_found += 1
         continue
 
-
-    # correlate Itunes ID with Navidrome ID (for use in a future script)
-    it_song_ID = int(it_song_entry.find('key', string='Track ID').next_sibling.text)
+    # correlate iTunes ID with Navidrome ID (for use in a future script)
+    it_song_ID = extract_int_key(it_song_entry, "Track ID", default=None)
     songID_correlation.update({it_song_ID: song_id})
-    
-    try:    # get rating, play count & date from Itunes
-        song_rating = int(it_song_entry.find('key', string='Rating').next_sibling.text)
-        song_rating = int(song_rating / 20)
-    except AttributeError: song_rating = 0 # rating = 0 (unrated) if it's not rated in itunes
-        
-    try:
-        play_count = int(it_song_entry.find('key', string='Play Count').next_sibling.text)
-        last_played = it_song_entry.find('key', string='Play Date UTC').next_sibling.text[:-1] # slice off the trailing 'Z'
-        last_played = datetime.datetime.strptime(last_played, '%Y-%m-%dT%H:%M:%S') # convert from string to datetime object. Example string: '2020-01-19T02:24:14Z'
-    except AttributeError: continue
 
-    update_playstats(artists, artist_id, play_count, last_played)
-    update_playstats(albums, album_id, play_count, last_played)
-    update_playstats(files, song_id, play_count, last_played, rating=song_rating)
+    song_info.it_rating = (
+        extract_int_key(it_song_entry, "Rating", default=0) // 20
+    )
+    song_info.it_album_rating = (
+        extract_int_key(it_song_entry, "Album Rating", default=0) // 20
+    )
+    song_info.it_play_count = extract_int_key(
+        it_song_entry, "Play Count", default=0
+    )
+    song_info.it_rated_at = extract_date_key(it_song_entry, "Date Modified")
+    song_info.it_play_date = extract_date_key(it_song_entry, "Play Date UTC")
 
-    
+    update_playstats(artists, artist_id, song_info, cur, "artist")
+    update_playstats(albums, album_id, song_info, cur, "album")
+    if song_id in files:
+        stats.skip_duplicated += 1
+    else:
+        files[song_id] = song_info
+
 
 conn.close()
 
-print('Writing changes to database:')
-write_to_annotation(artists, 'artist')
-print('Done writing artist records to database.')
-write_to_annotation(files, 'media_file')
-print('Done writing music file records to database.')
-write_to_annotation(albums, 'album')
-print('Album records saved to database.')
+print("Completed collecting songs information.")
 
-with open('IT_file_correlations.py', 'w') as f:
-    f.write('# Following python dictionary correlates the itunes integer ID to the Navidrome file ID for each song.\n')
-    f.write('# {ITUNES ID: ND ID} is the format. \n\n')
-    f.write('itunes_correlations = ')
+_ = None
+while _ != "proceed":
+    print()
+    print(
+        "This script will migrate certain data from your ITunes library to your Navidrome database.",
+        "Back up all your data in case it doesn't work properly on your setup. NO WARRANTIES. NO PROMISES.",
+        f"     Navidrome path: {nddb_path}",
+        f"               User: {userName} ({userID})",
+        f"iTunes library path: {itdb_path}",
+        f"              Stats: {stats}",
+        sep="\n",
+    )
+    print()
+
+    _ = input("Type PROCEED to continue, or Q to quit: ").lower()
+
+    if _ == "q":
+        print("Good bye.")
+        sys.exit(0)
+
+
+print("Writing changes to database:")
+write_to_annotation(artists, "artist")
+print("Done writing artist records to database.")
+write_to_annotation(files, "media_file")
+print("Done writing music file records to database.")
+write_to_annotation(albums, "album")
+print("Album records saved to database.")
+
+with open("IT_file_correlations.py", "w") as f:
+    f.write(
+        "# Following python dictionary correlates the itunes integer ID to the Navidrome file ID for each song.\n"
+    )
+    f.write("# {ITUNES ID: ND ID} is the format. \n\n")
+    f.write("itunes_correlations = ")
     f.write(pprint.pformat(songID_correlation))
 
-print('Navidrome database updated.')
-print(f"File correlation index saved to {str(Path.cwd() / 'IT_file_correlations.py')}\n")
-print('You can delete it if you want, but I will use it later in a script to transfer playlists from Itunes to Navidrome.')
+print("Navidrome database updated.")
+print(
+    f"File correlation index saved to {str(Path.cwd() / 'IT_file_correlations.py')}\n"
+)
+print(
+    "You can delete it if you want, but I will use it later in a script to transfer playlists from iTunes to Navidrome."
+)
